a96ce5c3e248fbf1dabe042ce84a5234df46a936
Juliette <juliette@aupluriel.be>
2022-08-01 10:43:34 +0200

    cloture

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -208,7 +208,13 @@
     </div>
   </div>
 </section>
+<section id="sect-video" >
+  <div class="video-container">
 
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/wQHz2n8EDw8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      
+  </div>
+</section>
 <section id="sect-partners"   class="scrollme">
   <div class="wrapper">
 
